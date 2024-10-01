cb67947d18b319577f7375bbe45925a31d88a4c3
mrityunjay9693 <mkaykumar123@gmail.com>
2024-10-01 13:15:17 +0530

    area and perimeter of square.

---
+++ Python/Basic/areaperimeterofsquare.py
@@ -0,0 +1,17 @@
+"""
+Problem: Write a Java program to read a side(length) of a square print the area and perimeter. 
+
+Input Format: Enter a side(length): side (any number) 
+Output Format:Area of square: area (any number, area of square)
+              Perimeter of square: perimeter (any number, perimeter of square)
+Sample Input: Enter side: 12 (any number)
+
+Sample Output: Area of square: 144
+               Perimeter of square: 48
+Area of square: side * side
+Perimeter of square: 4 * side 
+"""
+
+side = int(input("Enter side of square: "))
+print("Area of square:", (side * side))
+print("Perimeter of square:", (4 * side))
