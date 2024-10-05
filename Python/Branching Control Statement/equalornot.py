32d892ff597f52f3b4f59756a66d85c784860365
mrityunjay9693 <mkaykumar123@gmail.com>
2024-10-05 15:02:16 +0530

    Equal or Not Equal

---
+++ Python/Branching Control Statement/equalornot.py
@@ -0,0 +1,10 @@
+"""
+Write a python program to read two integer value from the user and print weather those are "Equal" or "Not Equal"
+"""
+num1 = int(input("ENter first number: "))
+num2 = int(input("Enter second number: "))
+
+if num1 == num2:
+    print("Equal.")
+else:
+    print("Not Equal.")
