416c2204cd3d1b6acd558048a0b7c4f84f76bd14
mrityunjay9693 <mkaykumar123@gmail.com>
2024-10-05 14:43:28 +0530

    positive or negative or zero

---
+++ Python/Branching Control Statement/positivenegativezero.py
@@ -0,0 +1,10 @@
+"""
+Write a python program to print user enter number is positive or negative or zero.
+"""
+number = int(input("Enter a number: "))
+if  number == 0:
+    print("Zero.")
+elif number > 0:
+    print("Positive.")
+else:
+    print("Negative.")
