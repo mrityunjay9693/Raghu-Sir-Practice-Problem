0edeed7386ff5a184172dab6a5223951585fd3bf
mrityunjay9693 <mkaykumar123@gmail.com>
2024-10-05 15:08:43 +0530

    biggest value among two given values.

---
+++ Python/Branching Control Statement/biggestnumber.py
@@ -0,0 +1,10 @@
+"""
+Write a python program to read two integer number from user and you have to print biggest among them. 
+"""
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+if num1 > num2:
+    print("Biggest number: ", num1)
+else:
+    print("Biggest number: ", num2)
